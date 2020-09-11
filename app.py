4ed07fbcfd3b23afa7689fab5ebbab13fadd7aab
loulunds <lousanne.lundstrom@gmail.com>
2020-09-11 09:40:50 +0000

    environment variable and templates added

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import os
-from flask import Flask
+from os import path
+if path.exists("env.py"):
+    import env
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] =  os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello, World...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks = mongo.db.tasks.find())
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
